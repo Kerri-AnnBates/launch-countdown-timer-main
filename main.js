52c98f903901f8cf136f83feda317959a6db4b5e
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-12-31 20:19:50 -0500

    day to start countdown

--- main.js
+++ main.js
@@ -3,4 +3,31 @@ const hourElement = document.querySelector("#hour");
 const minuteElement = document.querySelector("#minute");
 const secondElement = document.querySelector("#second");
 
-let startDay = 14;
+
+let hours = 24;
+let minutes = 59;
+let seconds = 60;
+let currentDate = new Date();
+
+currentDate.setDate(currentDate.getDate() + 14);
+let startDay = currentDate.getDate();
+
+
+// setInterval(() => {
+//     seconds -= 1;
+//     console.log(`Days: ${days} Hours: ${hours} Minutes: ${minutes} Seconds: ${seconds}`);
+
+//     if (seconds === 0) {
+//         seconds = 60;
+//         minutes -= 1;
+//     }
+
+//     if (minutes === 0) {
+//         hours -= 1;
+//     }
+
+//     if (hours === 0) {
+//         days -= 1;
+//     }
+
+// }, 1000);
