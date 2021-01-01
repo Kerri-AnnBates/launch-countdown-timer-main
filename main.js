833501bb93de331bc24b751315d5703c02ace3a3
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-12-31 19:53:28 -0500

    remove card for now

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("connected");
+const dayElement = document.querySelector("#day");
+const hourElement = document.querySelector("#hour");
+const minuteElement = document.querySelector("#minute");
+const secondElement = document.querySelector("#second");
+
+let startDay = 14;
