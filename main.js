85c75a89256397e6fce609ee061dda73d53f5859
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-01-01 17:04:06 -0500

    making progress with timer countdown from hours

--- main.js
+++ main.js
@@ -4,30 +4,25 @@ const minuteElement = document.querySelector("#minute");
 const secondElement = document.querySelector("#second");
 
 
-let hours = 24;
-let minutes = 59;
-let seconds = 60;
-let currentDate = new Date();
+let startDay = 14 * 24 * 60 * 60;
+let timer = 60 * 60 * 2;  // 2 hours
+let seconds;
+let minutes;
+let hours;
+let day;
 
-currentDate.setDate(currentDate.getDate() + 14);
-let startDay = currentDate.getDate();
+setInterval(() => {
+    hours = parseInt(timer / (60 * 60), 10);
+    minutes = (minutes > 60) ? parseInt(timer / 60, 10) : parseInt((timer / 60) - 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-// setInterval(() => {
-//     seconds -= 1;
-//     console.log(`Days: ${days} Hours: ${hours} Minutes: ${minutes} Seconds: ${seconds}`);
+    console.log(`${hours}:${minutes}:${seconds}`);
+    timer--;
 
-//     if (seconds === 0) {
-//         seconds = 60;
-//         minutes -= 1;
-//     }
-
-//     if (minutes === 0) {
-//         hours -= 1;
-//     }
-
-//     if (hours === 0) {
-//         days -= 1;
-//     }
-
-// }, 1000);
+    // if (--timer < 0) {
+    //     timer = duration;
+    // }
+}, 1000);
